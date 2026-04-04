2771a941e47160f20d4afa8a1e9e69af6c4af8ce
R3D <cristophermendezcervantes@gmail.com>
2026-04-04 17:27:27 +0100

    added administrative privileges over heroes

--- wiki/heroes/admin.py
+++ wiki/heroes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Person
 # Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mail', 'phone', 'experience', 'dnd','pathfinder', 'cthulhu', 'indie', 'others', 'none', 'interests', 'modality', 'first_term', 'second_term')
+    search_fields = ('mail','phone','modality')
+    list_filter = ('mail','experience','modality')
--- wiki/heroes/views.py
+++ wiki/heroes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from articles import models
-from . import models
+from .models import Person
 def show_form(request):
     return render(request, 'documents/form.html', {})
 
@@ -9,10 +9,27 @@ def show_success(request):
     return render(request, 'documents/form_success.html', {})
 
 def show_form(request):
-    print("ENTRANDO EN LA VISTA")
+    if request.method  == 'POST':
 
-    if request.method == 'POST':
-        print("POST DETECTADO")
+        person = Person.objects.create(
+                name=request.POST.get('name'),
+                mail=request.POST.get('mail'),
+                phone=request.POST.get('phone'),
+                experience=request.POST.get('experience'),
+                dnd=bool(request.POST.get('dnd')),
+                pathfinder=bool(request.POST.get('pathfinder')),
+                cthulhu=bool(request.POST.get('cthulhu')),
+                indie=bool(request.POST.get('indie')),
+                others=bool(request.POST.get('others')),
+                none=bool(request.POST.get('none')),
+
+                interests=request.POST.get('interests'),
+                modality=request.POST.get('modality'),
+
+                first_term=bool(request.POST.get('first_term')),
+                second_term=bool(request.POST.get('second_term')),
+            )
+
         return redirect("./success/")
 
     return render(request, 'documents/form.html')
